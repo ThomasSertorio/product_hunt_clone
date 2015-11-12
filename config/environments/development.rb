41a0805a238a1b36bd4cee12b8ffbd0538eaf287
Thomas Sertorio <thomas.sertorio@gmail.com>
2015-11-12 13:21:01 +0100

    React config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Config for react in dev mode
+  config.react.variant = :development
 end
