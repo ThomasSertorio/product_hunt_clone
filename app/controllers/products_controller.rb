bfbca3e72be9169a7a2a22dc1d063a31e6636f25
Thomas Sertorio <thomas.sertorio@gmail.com>
2015-11-12 15:09:02 +0100

    no react working with ajax

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,25 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:upvote]
 
   def index
     @products = Product.all
+  end
+
+  def upvote
+    if user_signed_in?
+      if current_user.voted_for? @product
+        current_user.unvote_for @product
+      else
+        current_user.up_votes @product
+      end
+    else
+      redirect_to new_user_session_path
+    end
+
+  end
+
+  private
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
